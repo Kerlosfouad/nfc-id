d87e7840fa501639af102aec7a82a50404a100a7
kerlos fouad <kokomx10@gmail.com>
2026-06-20 17:32:07 +0300

    fix: remove eslint from next.config, fix prerender issues

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "ui-avatars.com" },
       { protocol: "https", hostname: "**" },
     ],
   },
